dd175bdd1dbb8028a56fc3fa206d2d4089e63497
diksha <rajdiksha91@gmail.com>
2024-05-24 00:22:12 +0530

    news card design

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import ChannelSearch from "./components/ChannelSearch";
 import ChannelButton from "./components/ChannelButton";
 import Categories from "./components/Categories";
-// import NewsCard from "./components/NewsCard";
+import NewsCard from "./components/NewsCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Categories />
+      {/* <Categories />
       <ChannelSearch />
-      <ChannelButton />
+      <ChannelButton /> */}
+      <NewsCard />
     </>
   );
 }
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -2,65 +2,62 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const NewsCard = () => {
-  const NEWS_URL = "https://newsapi.org/v2/everything?q=everything";
-  const SOURCE_URL = "https://newsapi.org/v2/top-headlines/sources";
-  const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-
-  const [sources, setSources] = useState([]);
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchSources = async () => {
-      try {
-        const response = await fetch(`${SOURCE_URL}?apiKey=${API_KEY}`);
-        const result = await response.json();
-        setSources(result.sources);
-      } catch (error) {
-        console.error("Error fetching sources:", error);
-      }
-    };
-
-    const fetchArticles = async () => {
-      try {
-        const response = await fetch(`${NEWS_URL}&apiKey=${API_KEY}`);
-        const result = await response.json();
-        setArticles(result.articles);
-      } catch (error) {
-        console.error("Error fetching articles:", error);
-      }
-    };
-
-    const fetchData = async () => {
-      setLoading(true);
-      await fetchSources();
-      await fetchArticles();
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  const getCategoryBySourceId = (sourceId) => {
-    const source = sources.find((src) => src.id === sourceId);
-    return source ? source.category : "unknown";
+  const imgAdress = "./public/image.png";
+  const article = {
+    source: {
+      id: "wired",
+      name: "Wired",
+    },
+    author: "Aarian Marshall",
+    title: "Everything You Need to Know About Hybrid Cars",
+    description:
+      "What’s the difference between a hybrid, a mild hybrid, and a plug-in hybrid? This is the WIRED guide for the electric-curious.",
+    url: "https://www.wired.com/story/everything-you-need-to-know-about-hybrid-cars/",
+    urlToImage:
+      "https://media.wired.com/photos/663393c4547d38327b3c2a1e/191:100/w_1280,c_limit/Hybrid-Gars-Guide-Gear-GettyImages-1137081327.jpg",
+    publishedAt: "2024-05-03T09:30:00Z",
+    content:
+      "Have you heard?The hybrids are coming. As sales growth falters for electric vehicles, particularly in the United States, automakers have started to turn their gaze toward the EVs less-charged-up cous… [+2587 chars]",
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
   return (
     <>
-      <div>
-        {articles.map((article, index) => {
-          const category = getCategoryBySourceId(article.source.id);
-          return (
-            <div key={index} className="article">
-              <h2>{article.title}</h2>
-              <p>Category: {category}</p>
-            </div>
-          );
-        })}
+      <div className="flex p-2 gap-2 border border-color-1 rounded-lg m-1 sm:border-2 sm:px-4 sm:flex-col sm:w-1/3">
+        {article.urlToImage != null ? (
+          <img
+            src={article.urlToImage}
+            alt="image"
+            className="max-h-24 max-w-36 rounded sm:max-h-52 sm:max-w-fit"
+          />
+        ) : (
+          <img
+            src={imgAdress}
+            alt="image"
+            className="max-h-24 max-w-36 rounded sm:max-h-52 sm:max-w-fit"
+          />
+        )}
+        <div className="flex flex-col gap-1">
+          <h1 className="text-color-1 text-md font-bold leading-4 sm:leading-6 sm:text-xl">
+            {article.title}
+          </h1>
+          <div className="flex justify-between items-center">
+            <p className=" flex flex-col leading-4 text-slate-500 text-xs sm:text-sm">
+              {article.author}
+              <span className="font-bold text-color-1">
+                {article.source.name}
+              </span>
+            </p>
+            <p className="text-xs sm:text-slate-500">{article.publishedAt}</p>
+          </div>
+          <p className="hidden sm:block text-sm text-slate-700">
+            {article.description}
+          </p>
+          <a
+            href={article.url}
+            className="self-center px-10 bg-color-1 text-center text-white rounded"
+          >
+            Read More
+          </a>
+        </div>
       </div>
     </>
   );
