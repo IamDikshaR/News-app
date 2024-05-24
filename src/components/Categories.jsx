cc1c1bd87415d581a8ce6ea93b28c1cd2aa740bf
diksha <rajdiksha91@gmail.com>
2024-05-24 21:58:28 +0530

    responsive catgeory section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -36,6 +36,7 @@ const Categories = () => {
   //   </>
   // );
   const [category, setCategory] = useState("general");
+  const [show, setShow] = useState(false);
   const categories = [
     "general",
     "business",
@@ -63,7 +64,44 @@ const Categories = () => {
   }, []);
   return (
     <>
-      <div className="flex flex-nowrap my-4 mx-2 justify-around">
+      <div className="sm:hidden flex justify-end mx-2">
+        <i
+          className="material-icons text-4xl"
+          onClick={() => setShow((prevShow) => !prevShow)}
+        >
+          menu
+        </i>
+      </div>
+      <div>
+        <h3
+          className={`sm:hidden ${
+            !show
+              ? "text-center text-lg text-color-2 bg-color-2/[0.15]"
+              : "hidden"
+          }`}
+        >
+          {category}
+        </h3>
+        {show && (
+          <div className="flex flex-col items-center gap-2">
+            {display.map((category, index) => (
+              <button
+                key={index}
+                ref={index === 0 ? defaultFocusRef : null}
+                className={`py-2 text-xl hover:text-color-2 hover:underline hover:underline-offset-[15px] focus:text-color-2 focus:bg-color-2/[0.15] focus:w-1/6  focus:border-b-2 focus:border-color-2 focus:outline-none`}
+                onClick={() => {
+                  setCategory(categories[index]);
+                  setShow((prevShow) => !prevShow);
+                }}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="hidden sm:flex sm:flex-nowrap sm:my-4 sm:mx-2 sm:justify-around">
         {display.map((category, index) => (
           <button
             key={index}
@@ -75,7 +113,7 @@ const Categories = () => {
           </button>
         ))}
       </div>
-      <News category={category} />
+      {/* <News category={category} /> */}
     </>
   );
 };
