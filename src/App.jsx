aa5021e10b815abff3470a0236e5276c5295c75e
diksha <rajdiksha91@gmail.com>
2024-05-27 02:14:23 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import ChannelSearch from "./components/ChannelSearch";
 import ChannelButton from "./components/ChannelButton";
 import Categories from "./components/Categories";
 import NewsCard from "./components/NewsCard";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <Categories />
       <ChannelSearch />
       <ChannelButton />
+      {/* <div className="sticky bottom-2">
+        <i className="material-icons  bg-color-1 text-white py-2 px-2 rounded-[50%] hover:text-3xl hover:px-3 hover:shadow-md hover:shadow-slate-400">
+          arrow_upward
+        </i>
+      </div> */}
       {/* <NewsCard /> */}
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-color-bg sticky top-0 flex items-center gap-2 pr-4 border border-b-2 border-color-1/[0.3] h-16 justify-between">
+      <div className="flex items-center gap-2">
+        <img src="/Icon.svg" alt="icon" />
+        <h1 className="text-2xl font-bold">
+          <span className="text-color-1">News</span>
+          <span className="text-color-3">-</span>
+          <span className="text-color-2">opedia</span>
+        </h1>
+      </div>
+      <div className="text-lg text-color-1 gap-8 hidden sm:flex ">
+        <a href="#news" className="hover:text-xl hover:font-semibold">
+          News
+        </a>
+        <a href="#channels" className="hover:text-xl hover:font-semibold">
+          Channels
+        </a>
+        <a href="#about" className="hover:text-xl hover:font-semibold">
+          About
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
